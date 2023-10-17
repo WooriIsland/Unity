317f4a2a1f1a386d53f1e9ca2b8ba0da30bba451
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2023-10-17 21:08:26 +0900

    10/17

---
+++ Assets/LHS/02_Scripts/Map.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnSceneGUI()
+    {
+    }
+}
