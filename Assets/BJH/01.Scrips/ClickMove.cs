862e75b51e93f9023bb91d2c692523e750f86c19
Jihwan Byeon <wkdghk24@gmail.com>
2023-10-17 18:18:08 +0900

    [feat] move player using mouse
    - 마우스를 스크린에 클릭하면 플레이어가 이동합니다.
    - 플레이어 언팩하여 Player11에 담고 카메라를 설정했습니다.

---
+++ Assets/BJH/01.Scrips/ClickMove.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMove : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
--- Assets/BJH/01.Scrips/GameManager.cs
+++ Assets/BJH/01.Scrips/GameManager.cs
@@ -5,11 +5,12 @@ using Photon.Pun;
 
 public class GameManager : MonoBehaviour
 {
+    public Transform tr;
     // Start is called before the first frame update
     void Start()
     {
         // create player
-        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+        PhotonNetwork.Instantiate("m_11", Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
