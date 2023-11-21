5c44b37adbbe36ff5ae3055fc92306304f53ec2c
Jihwan Byeon <wkdghk24@gmail.com>
2023-11-22 00:52:38 +0900

    [FEAT] 수요일 빌드 시연 때 사용
    - 룸 연결 됨
    - 플레이어 카메라 구도 변경 및 플레이어 애니메이션, 중력 적용 풀림

---
+++ Assets/BJH/01.Scrips/LobbyManager.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+// 섬 등록
+//{
+//    "createdAt": "2023-11-21T04:36:06.582Z",
+//  "lastModifiedAt": "2023-11-21T04:36:06.582Z",
+//  "islandId": 0,
+//  "islandUniqueNumber": "string",
+//  "islandName": "string",
+//  "island_introduce": "string",
+//  "daysSinceCreation": 0,
+//  "secret": true
+//}
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public GameObject createIsland, islandSelect, islandCustom, islandCode;
+    string islandName;
+
+    private void Start()
+    {
+        createIsland.SetActive(false);
+
+        
+    }
+
+
+
+    // 방 생성
+    public void CreateRoom()
+    {
+        // 방 옵션 설정
+        RoomOptions roomOption = new RoomOptions();
+
+        // 공개, 비공개 여부
+        roomOption.IsVisible = true; // 임시
+
+        // 방 생성
+        islandName = islandCustom.GetComponent<CreateIslandInfo>().islandName.text;
+        InfoManager.Instance.IslandName = islandName; // 방 이름 info에 저장
+        InfoManager.Instance.IslandIntroduce = islandCustom.GetComponent<CreateIslandInfo>().introduce.text; // 방 설명 info에 저장
+        TypedLobby typedLobby = new TypedLobby("Woori Island", LobbyType.Default);
+        PhotonNetwork.CreateRoom(islandName, roomOption, typedLobby);
+    }
+    
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        print($"가족섬 생성 : {InfoManager.Instance.IslandName}");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        print($"가족섬 생성 실패 : {message}");
+    }
+
+
+    // 방 입장
+    public void JoinRoom(GameObject go)
+    {
+        string islandName = go.GetComponent<CreatedRoomInfo>().islandName.text;
+        PhotonNetwork.JoinRoom(islandName);
+        print(islandName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        print("방 입장 완료");
+
+        // 게임 씬으로 이동
+        SceneManager.LoadScene(3);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        base.OnJoinRoomFailed(returnCode, message);
+        print($"방 입장 실패 : {message}");
+    }
+
+    //public void OnClickJoinBtn(string code)
+    //{
+    //    //int num = UnityEngine.Random.Range(0, 100);
+    //    ////현숙 임시 구현(조건문으로 가야함)
+    //    //nickName = "정이" + num.ToString();
+
+    //    ////지환 구현 
+    //    ///*nickName = inputNickName.text;
+    //    //byte[] a = System.Text.Encoding.UTF8.GetBytes(nickName);
+    //    //familyCode = inputFamilyCode.text;*/
+
+    //    //PlayerPrefs.SetString("NickName", nickName);
+
+    //    //SceneManager.LoadScene(3);
+
+    //    //ConnectionManager03.Instance.JoinRoom(code);
+
+    //}
+
+
+
+
+
+    // 섬 생성 버튼 클릭
+    public void OnClick_CreateIsland()
+    {
+        createIsland.SetActive(true);
+    }
+
+    public void Onclick_SelectedIsland(string s)
+    {
+        InfoManager.Instance.IslandType = s;
+    }
+
+    public void OnClick_CustomIsland()
+    {
+        islandSelect.SetActive(false);
+        islandCustom.SetActive(true);
+    }
+
+    //public void Onclick_GetFamilyCode()
+    //{
+    //    // Custom에서 모든 정보 입력 후 다음 버튼을 누르면
+    //    // Custom했던 정보를 받고
+    //    // Custom에서 Code로 이동x
+    //    var islandInfo = islandCustom.GetComponent<CreateIslandInfo>();
+
+    //    InfoManager.Instance.IslandName = islandInfo.islandName.text;
+    //    InfoManager.Instance.IslandIntroduce = islandInfo.introduce.text;
+
+    //    islandCustom.SetActive(false);
+    //    islandCode.SetActive(true);
+
+    //    CreateFamilyCode();
+    //}
+
+
+
+    //public void OnClick_CloseCreateIsland()
+    //{
+    //    islandCode.SetActive(false);
+    //    createIsland.SetActive(false);
+
+    //    //ConnectionManager03.Instance.CreateRoom();
+    //}
+
+    //private void CreateFamilyCode()
+    //{
+    //    // 방 생성 구현되면 주석 풀기
+    //    //int minValue = 1;
+    //    //int maxValue = 100;
+    //    //string familyCode = "FamilyCode" + UnityEngine.Random.Range(minValue, maxValue);
+
+    //    string familyCode = "familycode123";
+    //    islandCode.GetComponent<CreateIslandInfo>().code.text = familyCode;
+    //    InfoManager.Instance.IslandCode = familyCode;
+    //}
+}
