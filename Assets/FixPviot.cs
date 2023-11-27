3646095cc76334afb9bdf21fa47faad451de9144
Jihwan Byeon <wkdghk24@gmail.com>
2023-11-27 18:10:01 +0900

    [FEAT] 애니메이션 수정 완

---
+++ Assets/FixPviot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPviot : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = new Vector3(0, -1f, 0);
+    }
+}
