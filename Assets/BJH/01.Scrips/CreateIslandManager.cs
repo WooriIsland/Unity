91fd1f466b1bffca35596c3220af0188b026f588
Jihwan Byeon <wkdghk24@gmail.com>
2023-12-02 01:12:49 +0900

    [FEAT] 로비씬 수정중
    - RoomItem 프리팹 생성
    - RoomItem에 CreatedRoomInfo.cs
    - Layout Group 변경

--- Assets/BJH/01.Scrips/CreateIslandManager.cs
+++ Assets/BJH/01.Scrips/CreateIslandManager.cs
@@ -110,7 +110,8 @@ public class CreateIslandManager : MonoBehaviour
         //HttpManager_LHS.instance.mainLoding.GetComponent<AlphaGPSSet>().OpenAlpha();
         
         // 방문 하고싶은 섬을 클릭하면? 해당하는 섬의 섬 이름, 섬 유형 저장
-        InfoManager.Instance.visit = go.GetComponent<CreatedRoomInfo>().islandName.text;
+        // islandName을 프리팹 info에 추가하면 주석 풀기
+        //InfoManager.Instance.visit = go.GetComponent<CreatedRoomInfo>().islandName.text;
         InfoManager.Instance.visitType = go.GetComponent<CreatedRoomInfo>().islandType.name;
 
         SceneManager.LoadScene(3);
--- Assets/BJH/01.Scrips/CreatedRoomInfo.cs
+++ Assets/BJH/01.Scrips/CreatedRoomInfo.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class CreatedRoomInfo : MonoBehaviour
 {
-    public TMP_Text islandName, like;
+    public TMP_Text roomName, roomInfo,familCnt, Days;
+    public TMP_Text likeCnt;
+    public GameObject Unlike;
     public GameObject islandType;
 }
